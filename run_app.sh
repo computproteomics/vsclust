e44fe36911474dcfe321e41b8a6b23880a263228
veitveit <veitveit@gmail.com>
2020-07-04 08:39:15 +0200

    now correctly running app

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,13 @@
-#!/bin/bash 
-R -e "shiny::runApp(port=3838)"
+#!/usr/bin/env Rscript
+
+currPath <- getwd()
+  initial.options <- commandArgs(trailingOnly = FALSE)
+  file.arg.name <- "--file="
+  script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+  script.basename <- dirname(script.name)
+  cat(paste0("Getting R functions from files located in ", script.basename),"\n")
+  setwd(script.basename)
+  shiny::runApp(port=3838)
+setwd(currPath)
+
 
